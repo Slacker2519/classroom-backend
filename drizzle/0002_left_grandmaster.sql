3d870f8ea5bd218d2077e8283f86cb83e500a38f
Slacker <99643664+Slacker2519@users.noreply.github.com>
2026-03-03 14:50:21 +0700

    generate the new sql again

---
+++ drizzle/0002_left_grandmaster.sql
@@ -0,0 +1,71 @@
+ALTER TABLE "classes" RENAME COLUMN "subjectId" TO "subject_id";--> statement-breakpoint
+ALTER TABLE "classes" RENAME COLUMN "teacherId" TO "teacher_id";--> statement-breakpoint
+ALTER TABLE "enrollments" RENAME COLUMN "studentId" TO "student_id";--> statement-breakpoint
+ALTER TABLE "enrollments" RENAME COLUMN "classId" TO "class_id";--> statement-breakpoint
+ALTER TABLE "subjects" RENAME COLUMN "departmentId" TO "department_id";--> statement-breakpoint
+ALTER TABLE "account" RENAME COLUMN "accountId" TO "account_id";--> statement-breakpoint
+ALTER TABLE "account" RENAME COLUMN "providerId" TO "provider_id";--> statement-breakpoint
+ALTER TABLE "account" RENAME COLUMN "userId" TO "user_id";--> statement-breakpoint
+ALTER TABLE "account" RENAME COLUMN "accessToken" TO "access_token";--> statement-breakpoint
+ALTER TABLE "account" RENAME COLUMN "refreshToken" TO "refresh_token";--> statement-breakpoint
+ALTER TABLE "account" RENAME COLUMN "idToken" TO "id_token";--> statement-breakpoint
+ALTER TABLE "account" RENAME COLUMN "accessTokenExpiresAt" TO "access_token_expires_at";--> statement-breakpoint
+ALTER TABLE "account" RENAME COLUMN "refreshTokenExpiresAt" TO "refresh_token_expires_at";--> statement-breakpoint
+ALTER TABLE "account" RENAME COLUMN "createdAt" TO "created_at";--> statement-breakpoint
+ALTER TABLE "account" RENAME COLUMN "updatedAt" TO "updated_at";--> statement-breakpoint
+ALTER TABLE "session" RENAME COLUMN "expiresAt" TO "expires_at";--> statement-breakpoint
+ALTER TABLE "session" RENAME COLUMN "createdAt" TO "created_at";--> statement-breakpoint
+ALTER TABLE "session" RENAME COLUMN "updatedAt" TO "updated_at";--> statement-breakpoint
+ALTER TABLE "session" RENAME COLUMN "ipAddress" TO "ip_address";--> statement-breakpoint
+ALTER TABLE "session" RENAME COLUMN "userAgent" TO "user_agent";--> statement-breakpoint
+ALTER TABLE "session" RENAME COLUMN "userId" TO "user_id";--> statement-breakpoint
+ALTER TABLE "user" RENAME COLUMN "emailVerified" TO "email_verified";--> statement-breakpoint
+ALTER TABLE "user" RENAME COLUMN "imageCldPubId" TO "image_cld_pub_id";--> statement-breakpoint
+ALTER TABLE "user" RENAME COLUMN "createdAt" TO "created_at";--> statement-breakpoint
+ALTER TABLE "user" RENAME COLUMN "updatedAt" TO "updated_at";--> statement-breakpoint
+ALTER TABLE "verification" RENAME COLUMN "expiresAt" TO "expires_at";--> statement-breakpoint
+ALTER TABLE "verification" RENAME COLUMN "createdAt" TO "created_at";--> statement-breakpoint
+ALTER TABLE "verification" RENAME COLUMN "updatedAt" TO "updated_at";--> statement-breakpoint
+ALTER TABLE "enrollments" DROP CONSTRAINT "enrollments_student_class_unique";--> statement-breakpoint
+ALTER TABLE "classes" DROP CONSTRAINT "classes_subjectId_subjects_id_fk";
+--> statement-breakpoint
+ALTER TABLE "classes" DROP CONSTRAINT "classes_teacherId_user_id_fk";
+--> statement-breakpoint
+ALTER TABLE "enrollments" DROP CONSTRAINT "enrollments_studentId_user_id_fk";
+--> statement-breakpoint
+ALTER TABLE "enrollments" DROP CONSTRAINT "enrollments_classId_classes_id_fk";
+--> statement-breakpoint
+ALTER TABLE "subjects" DROP CONSTRAINT "subjects_departmentId_departments_id_fk";
+--> statement-breakpoint
+ALTER TABLE "account" DROP CONSTRAINT "account_userId_user_id_fk";
+--> statement-breakpoint
+ALTER TABLE "session" DROP CONSTRAINT "session_userId_user_id_fk";
+--> statement-breakpoint
+DROP INDEX "account_userId_idx";--> statement-breakpoint
+DROP INDEX "account_provider_account_unique";--> statement-breakpoint
+DROP INDEX "session_userId_idx";--> statement-breakpoint
+DROP INDEX "session_token_unique";--> statement-breakpoint
+DROP INDEX "user_email_unique";--> statement-breakpoint
+DROP INDEX "classes_subject_id_idx";--> statement-breakpoint
+DROP INDEX "classes_teacher_id_idx";--> statement-breakpoint
+DROP INDEX "enrollments_student_id_idx";--> statement-breakpoint
+DROP INDEX "enrollments_class_id_idx";--> statement-breakpoint
+ALTER TABLE "classes" ALTER COLUMN "invite_code" SET DATA TYPE text;--> statement-breakpoint
+ALTER TABLE "enrollments" ADD CONSTRAINT "enrollments_student_id_class_id_pk" PRIMARY KEY("student_id","class_id");--> statement-breakpoint
+ALTER TABLE "classes" ADD CONSTRAINT "classes_subject_id_subjects_id_fk" FOREIGN KEY ("subject_id") REFERENCES "public"."subjects"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "classes" ADD CONSTRAINT "classes_teacher_id_user_id_fk" FOREIGN KEY ("teacher_id") REFERENCES "public"."user"("id") ON DELETE restrict ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "enrollments" ADD CONSTRAINT "enrollments_student_id_user_id_fk" FOREIGN KEY ("student_id") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "enrollments" ADD CONSTRAINT "enrollments_class_id_classes_id_fk" FOREIGN KEY ("class_id") REFERENCES "public"."classes"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "subjects" ADD CONSTRAINT "subjects_department_id_departments_id_fk" FOREIGN KEY ("department_id") REFERENCES "public"."departments"("id") ON DELETE restrict ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "account" ADD CONSTRAINT "account_user_id_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "session" ADD CONSTRAINT "session_user_id_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+CREATE INDEX "account_user_id_idx" ON "account" USING btree ("user_id");--> statement-breakpoint
+CREATE INDEX "session_user_id_idx" ON "session" USING btree ("user_id");--> statement-breakpoint
+CREATE INDEX "classes_subject_id_idx" ON "classes" USING btree ("subject_id");--> statement-breakpoint
+CREATE INDEX "classes_teacher_id_idx" ON "classes" USING btree ("teacher_id");--> statement-breakpoint
+CREATE INDEX "enrollments_student_id_idx" ON "enrollments" USING btree ("student_id");--> statement-breakpoint
+CREATE INDEX "enrollments_class_id_idx" ON "enrollments" USING btree ("class_id");--> statement-breakpoint
+ALTER TABLE "enrollments" DROP COLUMN "id";--> statement-breakpoint
+ALTER TABLE "enrollments" DROP COLUMN "created_at";--> statement-breakpoint
+ALTER TABLE "enrollments" DROP COLUMN "updated_at";--> statement-breakpoint
+ALTER TABLE "enrollments" ADD CONSTRAINT "enrollments_student_id_class_id_unique" UNIQUE("student_id","class_id");
